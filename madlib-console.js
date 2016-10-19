2abd1b852d226a4ce05be6d177b25ebbf4a68321
aaronkaltman <aaronkaltman@gmail.com>
2016-10-19 13:45:42 -0400

    added proper console.log to madlib-console.js

--- madlib-console.js
+++ madlib-console.js
@@ -3,4 +3,4 @@ var startupX = ["puppies","kittens","computers","keyboards","pens","calculators"
 var startupY =  ["cars","sloth","shoes", "music","backpacks","jeans","yoga","private","news"];
 
 
-console.log("A startup that is 'startupX' but for y");
+console.log("A startup that is %s but for %s",startupX[0],startupY[0]);
