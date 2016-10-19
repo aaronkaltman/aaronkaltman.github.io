157eb1253519fb837c185c913787592c2d94eafd
aaronkaltman <aaronkaltman@gmail.com>
2016-10-19 18:55:33 -0400

    addded some html

--- homework1/madlib-console.js
+++ homework1/madlib-console.js
@@ -4,7 +4,7 @@ var startupX = ["puppies","kittens","computers","keyboards","pens","calculators"
 var startupY =  ["cars","sloth","shoes", "music","backpacks","jeans","yoga","private","news"];
 
 //When the create new button is clicked - You clicked me should pop up
-document.getElementById("createnew").onclick = function() {myFunction()};
+document.getElementById("cnew").onclick = function() {myFunction()};
 
 function myFunction() {
     document.getElementById("XforY").innerHTML = "YOU CLICKED ME!!!";
