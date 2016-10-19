ec61b1057c0ab0c80718c898d4f3fa473c70a102
aaronkaltman <aaronkaltman@gmail.com>
2016-10-19 13:51:53 -0400

    added proper console.log to madlib-console.js

--- madlib-console.js
+++ madlib-console.js
@@ -1,6 +1,7 @@
+//Declare first array
 var startupX = ["puppies","kittens","computers","keyboards","pens","calculators","sports"];
-
+//Declare second array
 var startupY =  ["cars","sloth","shoes", "music","backpacks","jeans","yoga","private","news"];
 
-
+//Outputs a string containing two variables
 console.log("A startup that is %s but for %s",startupX[0],startupY[0]);
