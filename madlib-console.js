e4a0a607d45bd6d3d22f8c45e43fc06aedb78f11
aaronkaltman <aaronkaltman@gmail.com>
2016-10-19 13:54:40 -0400

    added function onclick

--- madlib-console.js
+++ madlib-console.js
@@ -3,5 +3,11 @@ var startupX = ["puppies","kittens","computers","keyboards","pens","calculators"
 //Declare second array
 var startupY =  ["cars","sloth","shoes", "music","backpacks","jeans","yoga","private","news"];
 
+document.getElementById("cnew").onclick = function() {myFunction()};
+
+function myFunction() {
+    document.getElementById("cnew").innerHTML = "YOU CLICKED ME!";
+}
+
 //Outputs a string containing two variables
 console.log("A startup that is %s but for %s",startupX[0],startupY[0]);
